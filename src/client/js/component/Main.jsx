add4537587398d14e2f4d11ebbe618eca427ee2f
Bill42362 <bill42362@gmail.com>
2021-01-31 00:17:15 +0800

    Add video element in Main.

--- src/client/js/component/Main.jsx
+++ src/client/js/component/Main.jsx
@@ -1,5 +1,5 @@
 // Main.jsx
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 import MediaStreamHandler from '../component/MediaStreamHandler.jsx';
@@ -7,8 +7,30 @@ import MediaStreamMonitor from '../component/MediaStreamMonitor.jsx';
 import GreenBlueChannel from '../component/GreenBlueChannel.jsx';
 
 const Main = () => {
-  const [mediaStream, setMediaStream] = useState(null);
-  const greenBlueChannelCanvas = useRef(null);
+  const [mediaStream, setMediaStream] = useState();
+  const sourceVideo = useRef();
+  const greenBlueChannelCanvas = useRef();
+
+  useEffect(() => {
+    const video = document.createElement('video');
+    // required to tell iOS safari we don't want fullscreen
+    video.setAttribute('playsinline', true);
+    sourceVideo.current = video;
+    return () => {
+      video.srcObject = null;
+      return video.pause();
+    };
+  }, []);
+
+  useEffect(() => {
+    sourceVideo.current.srcObject = mediaStream;
+    if (mediaStream) {
+      sourceVideo.current.play();
+    } else {
+      sourceVideo.current.pause();
+    }
+  }, [mediaStream]);
+
   return (
     <StyledMain>
       <ModuleWrapper>
