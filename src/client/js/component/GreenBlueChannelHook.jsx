f17b1248ffec67cc336a459e383dee4e9eaa20fa
Bill42362 <bill42362@gmail.com>
2021-02-02 16:53:37 +0800

    Done animator and createProgram() in GreenBlueChannelHook.

--- src/client/js/component/GreenBlueChannelHook.jsx
+++ src/client/js/component/GreenBlueChannelHook.jsx
@@ -5,10 +5,178 @@ import styled from 'styled-components';
 
 import ResetButtonStyle from '../style/ResetButtonStyle.js';
 
+const vertexShaderSource = `#version 300 es
+in vec4 aPosition;
+in vec2 aTextCoord;
+out vec2 vTextCoord;
+
+void main() {
+  gl_Position = aPosition;
+  vTextCoord = aTextCoord;
+}
+`;
+const fragmentShaderSource = `#version 300 es
+precision highp float;
+
+uniform sampler2D uSource;
+in vec2 vTextCoord;
+
+out vec4 outColor;
+
+void main() {
+  vec4 originColor = texture(uSource, vTextCoord);
+  float gbColor = 2.0 * originColor.b * originColor.g;
+  outColor = vec4(gbColor, gbColor, gbColor, originColor.a);
+}
+`;
+
+const createShader = (context, type, source) => {
+  const shader = context.createShader(type);
+  context.shaderSource(shader, source);
+  context.compileShader(shader);
+  return shader;
+};
+
+const createProgram = ({
+  context,
+  vertexShaderSource,
+  fragmentShaderSource,
+}) => {
+  if (!context || !vertexShaderSource || !fragmentShaderSource) {
+    return;
+  }
+  const vertexShader = createShader(
+    context,
+    context.VERTEX_SHADER,
+    vertexShaderSource
+  );
+  const fragmentShader = createShader(
+    context,
+    context.FRAGMENT_SHADER,
+    fragmentShaderSource
+  );
+  const program = context.createProgram();
+  context.attachShader(program, vertexShader);
+  context.attachShader(program, fragmentShader);
+  context.linkProgram(program);
+  const success = context.getProgramParameter(program, context.LINK_STATUS);
+  if (success) {
+    return { vertexShader, fragmentShader, program };
+  }
+  // eslint-disable-next-line no-console
+  console.error('Link failed:', context.getProgramInfoLog(program));
+  // eslint-disable-next-line no-console
+  console.error('vs info-log:', context.getShaderInfoLog(vertexShader));
+  // eslint-disable-next-line no-console
+  console.error('fs info-log:', context.getShaderInfoLog(fragmentShader));
+  context.deleteShader(vertexShader);
+  context.deleteShader(fragmentShader);
+  context.deleteProgram(program);
+  return;
+};
+
+const clearGlCore = ({
+  context,
+  vertexShader,
+  fragmentShader,
+  program,
+} = {}) => {
+  if (!context) {
+    return;
+  }
+  vertexShader && context.deleteShader(vertexShader);
+  fragmentShader && context.deleteShader(fragmentShader);
+  program && context.deleteProgram(program);
+};
+
 const GreenBlueChannelHook = ({ canvasRef, pixelSource }) => {
   const [shouldAnimate, setShouldAnimate] = useState(false);
-  const [maxFps, setMaxFps] = useState(30);
+  const [maxFps] = useState(30);
   const [fps, setFps] = useState(0);
+
+  const webGlContext = useRef();
+  const vertexShader = useRef();
+  const fragmentShader = useRef();
+  const program = useRef();
+  const animationFrame = useRef();
+  const lastFrameTimestamp = useRef(0);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return () =>
+        clearGlCore({
+          context: webGlContext.current,
+          vertexShader: vertexShader.current,
+          fragmentShader: fragmentShader.current,
+          program: program.current,
+        });
+    }
+
+    const rect = canvas.getBoundingClientRect();
+    canvas.height = rect.height;
+    canvas.width = rect.width;
+    webGlContext.current = canvas.getContext('webgl2');
+
+    const glCore = createProgram({
+      context: webGlContext.current,
+      vertexShaderSource,
+      fragmentShaderSource,
+    });
+    if (glCore) {
+      vertexShader.current = glCore.vertexShader;
+      fragmentShader.current = glCore.fragmentShader;
+      program.current = glCore.program;
+
+      return () =>
+        clearGlCore({
+          context: webGlContext.current,
+          vertexShader: vertexShader.current,
+          fragmentShader: fragmentShader.current,
+          program: program.current,
+        });
+    }
+  }, [canvasRef]);
+
+  useEffect(() => {
+    const nextFrame = () => {
+      const context = webGlContext.current;
+      if (!context || !pixelSource || !program.current) {
+        return;
+      }
+
+      console.log('nextFrame()');
+
+      // clear
+      context.clearColor(0, 0, 0, 1);
+      context.clear(context.COLOR_BUFFER_BIT);
+
+      // draw
+      context.useProgram(program.current);
+    };
+
+    const animate = timestamp => {
+      if (shouldAnimate) {
+        animationFrame.current = window.requestAnimationFrame(animate);
+      }
+      const minInterval = 1000 / maxFps;
+      const elapsed = timestamp - lastFrameTimestamp.current;
+      if (elapsed >= minInterval) {
+        lastFrameTimestamp.current = timestamp;
+        nextFrame();
+        setFps(Math.ceil(1000 / elapsed));
+      }
+    };
+
+    if (!shouldAnimate) {
+      window.cancelAnimationFrame(animationFrame.current);
+      return;
+    }
+
+    window.cancelAnimationFrame(animationFrame.current);
+    animationFrame.current = window.requestAnimationFrame(animate);
+    return () => window.cancelAnimationFrame(animationFrame.current);
+  }, [shouldAnimate, maxFps, pixelSource]);
 
   return (
     <StyledGreenBlueChannelHook>
@@ -31,7 +199,9 @@ const GreenBlueChannelHook = ({ canvasRef, pixelSource }) => {
 };
 
 GreenBlueChannelHook.propTypes = {
-  pixelSource: PropTypes.node,
+  pixelSource: PropTypes.shape({
+    current: PropTypes.node,
+  }),
   canvasRef: PropTypes.object.isRequired,
 };
 
@@ -40,10 +210,9 @@ GreenBlueChannelHook.defaultProps = {
 };
 
 const StyledGreenBlueChannelHook = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  padding: 8px;
+  border: solid 1px #4834d4;
+  border-radius: 8px;
+  overflow: hidden;
 `;
 
 const Header = styled.div`
