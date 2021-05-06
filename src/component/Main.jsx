a865e42100860856f87bf95fe29b17a1e17741dd
Bill Hsiao <bill.h@swag.live>
2021-05-06 17:00:29 +0800

    Add basic controls and animation to Main.

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,18 @@
 // Main.jsx
 import React from 'react';
 import styled from 'styled-components';
-import { Scene, WebGLRenderer, Color } from 'three';
+import throttle from 'lodash/throttle';
+import { Scene, WebGLRenderer, Color, Clock, PerspectiveCamera, AxesHelper } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export class Main extends React.PureComponent {
   canvas = React.createRef();
+  camera = null;
   scene = null;
   renderer = null;
+  controls = null;
+  clock = null;
+  nextTick = null;
 
   initThree = () => {
     const renderer = new WebGLRenderer({
@@ -19,16 +25,46 @@ export class Main extends React.PureComponent {
     this.handleWindowResize();
   };
 
-  handleWindowResize = () => {
-    const renderer = this.renderer;
-    const canvas = this.canvas.current;
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  tick = () => {
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+    this.nextTick = window.requestAnimationFrame(this.tick);
   };
+
+  handleWindowResize = throttle(() => {
+    if (!this.canvas.current) {
+      return;
+    }
+    const canvas = this.canvas.current;
+    const renderer = this.renderer;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    if (!this.camera) {
+      this.camera = new PerspectiveCamera(75, width / height, 0.1, 100);
+      this.camera.position.set(0, 0, -4);
+
+      this.controls = new OrbitControls(this.camera, canvas);
+      this.controls.enableDamping = true;
+    } else {
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+    }
+  }, 100);
 
   componentDidMount() {
     this.scene = new Scene();
+    this.scene.add(new AxesHelper(1));
     this.initThree();
+    this.tick();
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.cancelAnimationFrame(this.nextTick);
+    window.removeEventListener('resize', this.handleWindowResize);
   }
 
   render() {
