6865ab85fc4e26375988195d953f8556470249cb
Bill42362 <bill42362@gmail.com>
2021-05-14 12:07:07 +0800

    Add dat.gui.

--- src/client/js/component/Main.jsx
+++ src/client/js/component/Main.jsx
@@ -1,15 +1,33 @@
 // Main.jsx
 import React from 'react';
 import styled from 'styled-components';
+import * as dat from 'dat.gui';
 
 import loadScriptTag from '../resource/loadScriptTag.js';
 
 export class Main extends React.PureComponent {
   canvas = React.createRef();
+  gui = null;
+  controlObject = {
+    enableVideo: false,
+    effects: {},
+  };
+  controlUIObject = {
+    enableVideo: null,
+    Effects: null,
+    effects: {},
+  };
 
-  startDeepAR = () => {
-    // start video immediately after the initalization, mirror = true
-    this.deepAR.startVideo(true);
+  handleDeepARInited = () => {
+    this.controlUIObject.enableVideo = this.gui
+      .add(this.controlObject, 'enableVideo')
+      .onChange(() => {
+        if (this.controlObject.enableVideo) {
+          this.deepAR.startVideo(true);
+        } else {
+          this.deepAR.stopVideo();
+        }
+      });
     /*
     // load the aviators effect on the first face into slot 'slot'
     this.deepAR.switchEffect(0, 'slot', './aviators', function() {
@@ -19,6 +37,7 @@ export class Main extends React.PureComponent {
   };
 
   async componentDidMount() {
+    this.gui = new dat.GUI({ hideable: true, closed: false, closeOnTop: true });
     const canvas = this.canvas.current;
     await loadScriptTag({
       id: 'deepar-js',
@@ -32,11 +51,11 @@ export class Main extends React.PureComponent {
       canvas,
       segmentationInfoZip: 'lib/segmentation.zip',
       numberOfFaces: 1, // how many faces we want to track min 1, max 4
-      onInitialize: this.startDeepAR,
+      onInitialize: this.handleDeepARInited,
     });
-    // download the face tracking model
     this.deepAR.downloadFaceTrackingModel('model/models-68-extreme.bin');
   }
+
   render() {
     return (
       <StyledMain>
