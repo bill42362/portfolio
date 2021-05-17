a2f66d4a7434ea513d3d6cbd6104636b9ed37219
Bill42362 <bill42362@gmail.com>
2021-05-17 17:15:26 +0800

    Add controllers of Banuba.

--- src/client/js/component/Main.jsx
+++ src/client/js/component/Main.jsx
@@ -1,6 +1,8 @@
 // Main.jsx
 import React from 'react';
 import styled from 'styled-components';
+import * as dat from 'dat.gui';
+import throttle from 'lodash/throttle';
 
 import { Webcam, Player, Dom } from '../../../lib/banuba/bin/BanubaSDK.js';
 
@@ -8,10 +10,35 @@ const isProd = 'production' === process.env.NODE_ENV;
 const banubaFileBase = isProd ? 'lib/banuba/bin/' : '../../../lib/banuba/bin/';
 
 export class Main extends React.PureComponent {
+  state = { containerHeight: `${(window.innerWidth * 9) / 16}px` };
   banubaContainer = React.createRef();
+  gui = null;
+  isWebcamApplied = false;
+  webcam = null;
+  player = null;
+  controlObject = {
+    enableVideo: false,
+    effects: {},
+  };
+  controlUIObject = {
+    enableVideo: null,
+    Effects: null,
+    effects: {},
+  };
+
+  handleWindowResize = throttle(() => {
+    if (!this.banubaContainer.current) {
+      return;
+    }
+    const banubaContainer = this.banubaContainer.current;
+    const width = banubaContainer.clientWidth;
+    const height = (9 * width) / 16;
+    this.setState({ containerHeight: `${height}px` });
+  }, 100);
 
   async componentDidMount() {
-    const player = await Player.create({
+    this.gui = new dat.GUI({ hideable: true, closed: false, closeOnTop: true });
+    this.player = await Player.create({
       clientToken: process.env.BANUBA_TOKEN,
       locateFile: {
         'BanubaSDK.data': `${banubaFileBase}BanubaSDK.data`,
@@ -19,14 +46,38 @@ export class Main extends React.PureComponent {
         'BanubaSDK.simd.wasm': `${banubaFileBase}BanubaSDK.simd.wasm`,
       },
     });
-    player.use(new Webcam());
-    Dom.render(player, this.banubaContainer.current);
+    this.webcam = new Webcam();
+    Dom.render(this.player, this.banubaContainer.current);
+
+    this.controlUIObject.enableVideo = this.gui
+      .add(this.controlObject, 'enableVideo')
+      .onChange(() => {
+        if (this.controlObject.enableVideo) {
+          if (!this.isWebcamApplied) {
+            this.player.use(this.webcam);
+            this.isWebcamApplied = true;
+          }
+          this.webcam.start();
+          this.player.play();
+        } else {
+          this.player.pause();
+          this.webcam.stop();
+        }
+      });
+
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    this.gui.destory();
+    window.removeEventListener('resize', this.handleWindowResize);
   }
 
   render() {
+    const { containerHeight } = this.state;
     return (
       <StyledMain>
-        <BanubaContainer ref={this.banubaContainer} />
+        <BanubaContainer height={containerHeight} ref={this.banubaContainer} />
       </StyledMain>
     );
   }
@@ -36,9 +87,10 @@ const StyledMain = styled.div`
   flex: auto;
 `;
 
-const BanubaContainer = styled.div`
+const BanubaContainer = styled.div.attrs(({ height }) => ({
+  style: { height },
+}))`
   width: 100%;
-  height: 100%;
   background-color: #222f3e;
 `;
 
