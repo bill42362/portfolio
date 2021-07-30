07512f93265b63b5ee6350b5f882e3e39947295a
Bill42362 <bill42362@gmail.com>
2021-07-31 03:34:10 +0800

    Add MovingLeastSquare.

---
+++ src/client/js/resource/MovingLeastSquare.js
@@ -0,0 +1,116 @@
+// MovingLeastSquare.js
+import {
+  getPointsVector2D,
+  getVectorLength2D,
+} from '../resource/getFaceMeshTransform.js';
+
+const STRIP_COUNT = 5;
+const STEP = 1 / STRIP_COUNT;
+
+/*
+const averageDots2D = ({ dots }) => {
+  return [
+    dots.reduce((cur, d) => cur + d[0], 0) / dots.length,
+    dots.reduce((cur, d) => cur + d[1], 0) / dots.length,
+  ];
+};
+*/
+const aDotB2D = ({ a, b }) => a[0] * b[0] + a[1] * b[1];
+
+const getMlsData = ({ pointPairs = [], alpha = 1 } = {}) => {
+  const result = new Array(STRIP_COUNT * STRIP_COUNT).fill(0);
+
+  result.forEach((_, index) => {
+    const position = [
+      STEP * (index % STRIP_COUNT),
+      STEP * Math.floor(index / STRIP_COUNT),
+    ];
+
+    const weights = pointPairs.map(({ origin }) => {
+      return (
+        1 /
+        Math.pow(
+          getVectorLength2D({
+            vector: getPointsVector2D({
+              origin: position,
+              target: origin,
+            }),
+          }),
+          2 * alpha
+        )
+      );
+    });
+
+    const weightsSum = weights.reduce((cur, w) => cur + w, 0);
+
+    const pStar = pointPairs.reduce(
+      (cur, p) => {
+        return [
+          (cur[0] + p.origin[0]) / weightsSum,
+          (cur[1] + p.origin[1]) / weightsSum,
+        ];
+      },
+      [0, 0]
+    );
+    const qStar = pointPairs.reduce(
+      (cur, p) => {
+        return [
+          (cur[0] + p.target[0]) / weightsSum,
+          (cur[1] + p.target[1]) / weightsSum,
+        ];
+      },
+      [0, 0]
+    );
+    const normalVector = getPointsVector2D({ origin: pStar, target: position });
+    const normalDistance = getVectorLength2D({ vector: normalVector });
+
+    const pHats = pointPairs.map(p =>
+      getPointsVector2D({
+        origin: pStar,
+        target: p.origin,
+      })
+    );
+    const qHats = pointPairs.map(p =>
+      getPointsVector2D({
+        origin: qStar,
+        target: p.target,
+      })
+    );
+
+    const As = weights.map((w, index) => {
+      const pHat = pHats[index];
+      const minusPHatOrth = [pHat[1], -pHat[0]];
+      const minusNormalVectorOrth = [normalVector[1], -normalVector[0]];
+      return [
+        w * aDotB2D({ a: pHat, b: normalVector }),
+        w * aDotB2D({ a: pHat, b: minusNormalVectorOrth }),
+        w * aDotB2D({ a: minusPHatOrth, b: normalVector }),
+        w * aDotB2D({ a: minusPHatOrth, b: minusNormalVectorOrth }),
+      ];
+    });
+
+    const fvVector = qHats.reduce(
+      (cur, qHat, index) => {
+        const A = As[index];
+        return [
+          cur[0] + qHat[0] * A[0] + qHat[1] * A[2],
+          cur[1] + qHat[0] * A[1] + qHat[1] * A[3],
+        ];
+      },
+      [0, 0]
+    );
+
+    const fvLength = getVectorLength2D({ vector: fvVector });
+
+    const newPosition = [
+      (normalDistance * fvVector[0]) / fvLength + qStar[0],
+      (normalDistance * fvVector[1]) / fvLength + qStar[1],
+    ];
+
+    result[index] = { position, newPosition };
+  });
+
+  return result;
+};
+
+export default getMlsData;
