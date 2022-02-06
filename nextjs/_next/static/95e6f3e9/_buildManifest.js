0cc40947ac4e8d3b5cc5d58fbd6705e03054d5af
bill42362 <bill42362@gmail.com>
2022-02-06 05:43:43 +0000

    Deploying to gh-pages from @ bill42362/portfolio@b328040bd3b4c1e47b70a4cd3b99bff3f4e68679 🚀

--- nextjs/_next/static/95e6f3e9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cd6bc2731f6f4592.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
