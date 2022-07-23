2433f727f269d3028cf6b7c41f270a586901936c
Bill42362 <bill42362@gmail.com>
2022-07-23 20:34:09 +0800

    Fix beforeInteractive <Script>.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,7 @@
 // _app.js
 import Head from 'next/head';
-import Script from 'next/script';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import styledNormalize from 'styled-normalize';
-import serialize from 'serialize-javascript';
 
 import env from '../resource/env.js';
 
@@ -108,16 +106,6 @@ function App({ Component, pageProps }) {
         <meta property="git:short_sha" content={env.shortSha} />
         <meta property="git:tag_name" content={env.tagName} />
       </Head>
-      <Script
-        type="text/javascript"
-        id="ssr-enviroment"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-              window.__SSR_ENVIRONMENT__ = ${serialize(env)};
-            `,
-        }}
-      />
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,10 @@
 // _document.js
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import { ServerStyleSheet } from 'styled-components';
+import serialize from 'serialize-javascript';
+
+import env from '../resource/env.js';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,5 +30,31 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <Script
+            type="text/javascript"
+            id="ssr-enviroment"
+            // beforeInteractive script must put in _document.js's <Head>
+            // and only works in next.js above 12.1.7
+            // https://github.com/vercel/next.js/issues/37741#issuecomment-1157358712
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.__SSR_ENVIRONMENT__ = ${serialize(env)};
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
