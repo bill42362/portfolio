49a7cbaa5175c0af3a5616135ae8e40f475860fa
Bill42362 <bill42362@gmail.com>
2022-07-23 21:42:34 +0800

    Fix lint error.

--- src/resource/isWebAssemblySupported.js
+++ src/resource/isWebAssemblySupported.js
@@ -7,11 +7,13 @@ const isWebAssemblySupported = (() => {
       typeof WebAssembly === 'object' &&
       typeof WebAssembly.instantiate === 'function'
     ) {
-      const module = new WebAssembly.Module(
+      const wasmModule = new WebAssembly.Module(
         Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
       );
-      if (module instanceof WebAssembly.Module)
-        return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+      if (wasmModule instanceof WebAssembly.Module)
+        return (
+          new WebAssembly.Instance(wasmModule) instanceof WebAssembly.Instance
+        );
     }
     // eslint-disable-next-line no-empty
   } catch (e) {}
